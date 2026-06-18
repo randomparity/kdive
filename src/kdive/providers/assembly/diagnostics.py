be03b4db53214aee006e9012cc48c19fee8d18ba
David Christensen <randomparity@gmail.com>
2026-06-18 02:11:30 -0700

    refactor: move remote diagnostics contribution

--- src/kdive/providers/assembly/diagnostics.py
+++ src/kdive/providers/assembly/diagnostics.py
@@ -3,7 +3,9 @@
 from __future__ import annotations
 
 from kdive.diagnostics.provider_contracts import DiagnosticProviderContribution
-from kdive.providers.remote_libvirt.diagnostics import diagnostic_contribution as remote_diagnostics
+from kdive.providers.remote_libvirt.diagnostics.contribution import (
+    diagnostic_contribution as remote_diagnostics,
+)
 
 
 def diagnostic_provider_contributions() -> tuple[DiagnosticProviderContribution, ...]:
