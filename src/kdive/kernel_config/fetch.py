3733a3309bcd567934d067aba096bddbc4b87ad9
David Christensen <randomparity@gmail.com>
2026-07-11 10:02:34 -0700

    fix: remove kernel config service import

--- src/kdive/kernel_config/fetch.py
+++ src/kdive/kernel_config/fetch.py
@@ -18,9 +18,9 @@ import psycopg
 from psycopg import AsyncConnection
 
 from kdive.artifacts.storage import FetchedArtifact
+from kdive.db.artifact_queries import effective_config_key
 from kdive.domain.errors import CategorizedError
 from kdive.kernel_config.parse import KernelConfig, parse_kernel_config
-from kdive.services.artifacts.read_model import effective_config_key
 from kdive.store.objectstore import object_store_from_env
 
 _log = logging.getLogger(__name__)
